2914ed607f1ff7e2d33a8195cda3b37c65a9438d
Prakash-Arulraj <tp612000@gmail.com>
2023-07-20 21:07:51 +0530

    new conflict

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -5,8 +5,13 @@ private void dummy() {
 	// TODO Auto-generated method stub
     System.out.println("Dummy");
 }
+private void fb() {
+	// TODO Auto-generated method stub
+System.out.println("Conflict new");
+}
 public static void main(String[] args) {
 	Sample s=new Sample();
 	s.dummy();
+	s.fb();
 }
 }
