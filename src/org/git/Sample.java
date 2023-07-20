e757bd7698ee827601b6e2f60f452151fff54442
Prakash-Arulraj <tp612000@gmail.com>
2023-07-20 20:24:05 +0530

    new

---
+++ src/org/git/Sample.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Sample {
+private void dummy() {
+	// TODO Auto-generated method stub
+    System.out.println("Dummy");
+}
+public static void main(String[] args) {
+	Sample s=new Sample();
+	s.dummy();
+}
+}
